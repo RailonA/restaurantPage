dfcdd520b7a5ab834f25965c35715928cde9b22b
Carlos Ospina <carlossospinag@gmail.com>
2021-05-28 16:16:49 -0500

    add header, body, footer to main page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
-
+// <body>
 //   <div id="content">
 //   </div>
+// </body>
+const header = document.createElement('header');
+const contentSelector = document.querySelector('#content');
+header.innerHTML = `<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<div class="container-fluid">
 
-// class RestaurentPage {
+  <div class="collapse navbar-collapse justify-content-md-center">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#" id="home-link">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" id="menu-link">Menu</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" id="contact-link">Contact Us</a>
+      </li>
+    </ul>
+  </div>
 
-// }
-const pageContent = document.getElementById("content");
+</div>
+</nav>`;
 
-pageContent.textContent = "board";
+contentSelector.append(header);
 
+const main = document.createElement('main');
+contentSelector.append(main);
 
+const footer = document.createElement('footer');
 
-console.log("Hello World!");
+footer.classList.add('py-4','bg-dark','flex-shrink-0');
+footer.innerHTML=`
+<div class="container text-center">
+  <a href='https://www.freepik.com/photos/background' class='text-muted'>This is the footer</a>
+</div>`;
+
+contentSelector.append(footer);
