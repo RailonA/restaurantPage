bed2c9a6c3f9496a61ea20cec71428be26e7bc98
Carlos Ospina <carlossospinag@gmail.com>
2021-05-30 01:58:07 -0500

    fix navbar

--- src/index.js
+++ src/index.js
@@ -17,25 +17,25 @@
 
 const header = document.createElement('header');
 const contentSelector = document.querySelector('#content');
-header.innerHTML = `<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-<div class="container-fluid">
-
-  <div class="collapse navbar-collapse justify-content-md-center">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="#" id="home-link">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#" id="menu-link">Menu</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#" id="contact-link">Contact Us</a>
-      </li>
-    </ul>
-  </div>
-
-</div>
-</nav>`;
+header.innerHTML = `
+<nav class="navbar navbar-expand navbar-dark bg-dark">
+    <div class="container-fluid">
+      <div class="collapse navbar-collapse justify-content-center">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#" id="home-link">Menu</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" id="menu-link">Menu</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" id="contact-link">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+`;
 
 contentSelector.append(header);
 
