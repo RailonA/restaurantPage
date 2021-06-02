723b1315c3ed44ae0f547b369ac1be386079a786
RailonA <railonacosta@gmail.com>
2021-06-02 09:30:47 -0400

    Fix contact page on live

--- src/menu.js
+++ src/menu.js
@@ -7,6 +7,7 @@ const renderMenuPage = (() => ({
     const bodySel = document.querySelector('body');
     bodySel.classList.add('bodyBackground2');
     main.classList.add('mx-auto');
+    main.classList.add('my-2', 'col-8', 'd-flex', 'flex-wrap', 'justify-content-center');
     homeLink.classList.remove('active');
     menuLink.classList.add('active');
     contactLink.classList.remove('active');
@@ -23,7 +24,6 @@ const renderMenuPage = (() => ({
     const cardTitle = document.createElement('h4');
     const cardText = document.createElement('h5');
     const priceTag = document.createElement('h5');
-    main.classList.add('my-2', 'col-8', 'd-flex', 'flex-wrap', 'justify-content-center');
     divCard.classList.add('col-4', 'background', 'm-4');
     cardImage.src = picture;
     cardImage.classList.add('rounded-circle', 'd-flex', 'justify-content-center', 'col-6', 'ml-50');
